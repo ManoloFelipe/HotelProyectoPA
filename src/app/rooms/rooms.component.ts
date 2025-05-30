b814bd404d0370e8e33524fdd6bff411be4e189e
ManoloFelipe <manolofelipec74@gmail.com>
2025-05-30 17:37:43 -0600

    fix room carousel

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -9,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent {
-
+  habitaciones = [
+    {
+      nombre: 'Habitación Individual',
+      descripcion: 'Diseñada para ejecutivos o viajeros solos. Estilo minimalista elegante con tecnología moderna.',
+      imagen: 'assets/habitaciones/individual.png'
+    },
+    {
+      nombre: 'Habitación Doble',
+      descripcion: 'Ideal para parejas o amigos. Estética simétrica con tonos azul y beige.',
+      imagen: 'assets/habitaciones/doble.png'
+    },
+    {
+      nombre: 'Habitación Familiar',
+      descripcion: 'Perfecta para familias con niños. Incluye litera, cama queen, y zona de juegos.',
+      imagen: 'assets/habitaciones/familiar.png'
+    },
+    {
+      nombre: 'Suite Principal',
+      descripcion: 'Espacio exclusivo para ejecutivos de lujo. Cuenta con cama king, sala lounge y escritorio de mármol.',
+      imagen: 'assets/habitaciones/suite.png'
+    }
+  ];
 }
